8e41556fa94e807ea6b9372aa335fedfce636269
Animesh Pareek <108825332+Mr-AniP@users.noreply.github.com>
2023-10-19 02:21:27 +0530

    spice 7 final

---
+++ desktop/build/resources/main/Background4/Background4/back4tiles1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="back4tiles1" tilewidth="16" tileheight="16" tilecount="9344" columns="146">
+ <image source="181b8c8f33fe852f398e346f13ccda8e.jpeg" width="2340" height="1032"/>
+</tileset>
---
+++ desktop/build/resources/main/Background5/Background5/back5tile1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="back5tile1" tilewidth="16" tileheight="16" tilecount="180" columns="18">
+ <image source=".jpg" width="300" height="168"/>
+</tileset>

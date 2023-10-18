2a052e39284d092a2c37c648869d3aff38b2cb08
Animesh Pareek <108825332+Mr-AniP@users.noreply.github.com>
2023-10-19 01:00:32 +0530

    Add files via upload

---
+++ assets/Background5/Background5/back5tile2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="back5tile2" tilewidth="16" tileheight="16" tilecount="3150" columns="75">
+ <image source="Tanstarsback2.png" width="1200" height="675"/>
+</tileset>
---
+++ assets/Background6/Background6/back6tiles1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="back6tiles1" tilewidth="16" tileheight="16" tilecount="8040" columns="120">
+ <image source="ded00065333621.5af0ea8ca2b29.png" width="1920" height="1080"/>
+</tileset>
